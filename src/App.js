d1faaac7fa6209a0c668f0da4b1e78dc43c4df03
AnnaFutryk <anna.futryk@gmail.com>
2023-11-16 23:18:27 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// import NotFoundPage from "pages/NotFoundPage/NotFoundPage";
+import { lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
+const Catalog = lazy(() => import("./pages/Catalog/Catalog.jsx"));
+const Favorite = lazy(() => import("./pages/Favorites/Favorites.jsx"));
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="catalog" element={<Catalog />} />
+        <Route path="favorites" element={<Favorite />} />
+        {/* <Route path="*" element={<NotFoundPage />} /> */}
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
