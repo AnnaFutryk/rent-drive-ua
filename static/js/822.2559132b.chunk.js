c08101f9b8ce7be49754fac4d1ef590f2ee8814e
AnnaFutryk <anna.futryk@gmail.com>
2023-11-18 01:13:59 +0200

    Updates

--- static/js/822.2559132b.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkrent_drive_ua=self.webpackChunkrent_drive_ua||[]).push([[822],{822:(e,r,s)=>{s.r(r),s.d(r,{default:()=>t});var n=s(391),i=s(512),d=s(184);const t=()=>{const{data:e,error:r,isLoading:s}=(0,n.JB)();return(0,d.jsxs)(d.Fragment,{children:[r&&(0,d.jsx)("p",{children:"Ooops... something went wrong"}),s?(0,d.jsx)(i.Z,{}):(0,d.jsx)("ul",{children:e.map((e=>(0,d.jsxs)("li",{children:[(0,d.jsx)("img",{src:e.img,alt:"Car",width:250}),(0,d.jsx)("p",{children:e.model})]},e.id)))})]})}}}]);
-//# sourceMappingURL=822.2559132b.chunk.js.map
