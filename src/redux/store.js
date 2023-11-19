e1dafa08ca647f112e53d4ce11a5fe0c2f6b1f20
AnnaFutryk <anna.futryk@gmail.com>
2023-11-19 13:43:12 +0200

    add favorities logic

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,41 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+import storage from "redux-persist/lib/storage";
 import { carsApi } from "./carsSlice";
+import { favoriteReducer } from "./favoritesSlice";
+
+const reducers = combineReducers({
+  [carsApi.reducerPath]: carsApi.reducer,
+  favorites: favoriteReducer,
+});
+
+const persistConfig = {
+  key: "favorites",
+  version: 1,
+  storage,
+  whitelist: ["favorites"],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-  reducer: {
-    [carsApi.reducerPath]: carsApi.reducer,
-  },
+  reducer: persistedReducer,
 
-  middleware: (getDefaultMiddleware) => [
-    ...getDefaultMiddleware(),
-    carsApi.middleware,
-  ],
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(carsApi.middleware),
 });
+
+export const persistor = persistStore(store);
