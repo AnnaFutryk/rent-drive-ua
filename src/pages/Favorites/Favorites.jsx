193e6ae3e59cf18adcdd3d94041f6f5788aabd34
AnnaFutryk <anna.futryk@gmail.com>
2023-11-19 17:31:11 +0200

    add favorites page

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,7 +1,43 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import LoadMoreBtn from "../../components/Button/LoadMoreBtn";
+import CarItem from "../../components/CarItem/CarItem";
+
+import { getFavorites } from "../../redux/selectors";
+import { Text, Wrapper, WrapperList } from "./Favorites.styled";
+
 const Favorites = () => {
+  const [page, setPage] = useState(1);
+  const itemsPerPage = 12;
+  const favoriteCars = useSelector(getFavorites);
+
+  const handleLoadMore = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const displayedFavorites = favoriteCars.favorites.slice(startIndex, endIndex);
+  const hasMoreCars = favoriteCars.favorites.length > endIndex;
+
   return (
     <>
-      <p>Hello! It is Favorites</p>
+      <Wrapper>
+        <WrapperList>
+          {displayedFavorites.map((favorite, id) => (
+            <CarItem key={id} cars={favorite} />
+          ))}
+          {favoriteCars.favorites.length === 0 && (
+            <Text>
+              🚘 Your favorites list is currently empty. Add some cars to create
+              your personalized collection! 🚘
+            </Text>
+          )}
+        </WrapperList>
+        {hasMoreCars && (
+          <LoadMoreBtn onClick={handleLoadMore} text="Load more" />
+        )}
+      </Wrapper>
     </>
   );
 };
---
+++ src/pages/Favorites/Favorites.styled.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding-left: 128px;
+  padding-right: 128px;
+  padding-top: 60px;
+`;
+
+export const WrapperList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 50px 29px;
+  margin-top: 50px;
+  width: 1184px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const Text = styled.p`
+  width: 700px;
+  margin-top: 120px;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+  font-size: 36px;
+  font-style: normal;
+  font-weight: 500;
+  color: #3470ff;
+`;
