f5db5d3551e583697748ec44ae920d23a3aff728
AnnaFutryk <anna.futryk@gmail.com>
2023-11-20 00:22:58 +0200

    Updates

--- static/js/733.7ce64e9f.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkrent_drive_ua=self.webpackChunkrent_drive_ua||[]).push([[733],{733:(e,r,s)=>{s.r(r),s.d(r,{default:()=>a});var t=s(184);const a=()=>(0,t.jsx)(t.Fragment,{children:(0,t.jsx)("p",{children:"Hello! It is Favorites"})})}}]);
-//# sourceMappingURL=733.7ce64e9f.chunk.js.map
