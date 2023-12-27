db7d87592b8eee302f3b38933ed46e7f9ecdbc43
AnnaFutryk <anna.futryk@gmail.com>
2023-12-27 16:58:22 +0200

    delete coments

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,7 @@
-// import { ToastContainer, toast, Slide } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-// import Loader from 'components/Loader/Loader';
 import BgVideo from "../../video/IMG_1236.MP4";
 import { OverLay, Text, VideoBg } from "./HomePage.styled";
-// import { useLocation } from "react-router-dom";
 
 const HomePage = () => {
-  //отримуємо обʼєкт місця положення, щоб при кліку на Go back з інф про акторів-відгуків, було коректне повернення на HomePage
-  // const location = useLocation();
-
   return (
     <>
       <OverLay>
